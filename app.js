bbe7c15d018207743ec900782bc1d961a9497177
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-07-15 21:08:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express(); //creates express-app-object
+
+app.use(express.static(__dirname + '/public')); // public-foder
+
+const port = 3000; // use port 3000
+
+// root-folder
+app.get('/', function (req, res) {
+  console.log('A request was made to the root');
+  res.send('A request was made to the root 🤪');
+});
+
+app.listen(port, function () {
+  console.log(`Server running and listening on port ${port}`);
+});
